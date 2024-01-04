10f1c11314f42977e53433684cf7a4f5d5f54a8a
junjun.dong <dongjunjun1013@126.com>
2024-01-04 17:41:11 +0800

    to 4.4

---
+++ HelloWindow.cpp
@@ -0,0 +1,56 @@
+#include <chrono>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <thread>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    // initialize GLFW
+    glfwInit();
+    // configure GLFW
+    // tell GLFW 3.3 is the OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // explicitly use the core-profile,
+    // Telling GLFW we want to use the core-profile means
+    // we'll get access to a smaller subset of OpenGL features
+    // without backwards-compatible features we no longer need
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create a window object
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // [4.1 GLAD]: managers function pointers for OpenGL
+    // initialize GLAD before we call any OpenGL function
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // [4.2 Viewport]
+    // tell OpenGL the size of the rendering window
+    glViewport(0, 0, 800, 600);
+
+    // resize window the viewport should be adjusted as well
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // [4.3 Ready engines]
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
