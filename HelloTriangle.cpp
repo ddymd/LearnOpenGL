256777aba223111cab68d1e73aa33075fc9b12e3
junjun.dong <dongjunjun1013@126.com>
2024-07-10 16:35:03 +0800

    update learn opengl

--- HelloTriangle.cpp
+++ HelloTriangle.cpp
@@ -1,36 +1,71 @@
-#include <cstddef>
-#include <glad/glad.h>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include <spdlog/spdlog.h>
+#include <spdlog/cfg/env.h>
+#include <spdlog/logger.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
-#include <iostream>
+void test() {
+    SPDLOG_ERROR("error");
+    SPDLOG_CRITICAL("critical");
+    SPDLOG_WARN("warn");
+    SPDLOG_INFO("info");
+    SPDLOG_DEBUG("debug");
+    SPDLOG_TRACE("trace");
+}
 
-int main() {
-    glfwInit();
+void init_logger() {
+    auto logger = std::make_shared<spdlog::logger>("console", std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    spdlog::set_default_logger(logger);
+    spdlog::cfg::load_env_levels();
+    spdlog::set_pattern("[%^%L%$][%t][%H:%M:%S.%e][%s:%# %!()] %v");
+}
+
+void glfw_error_callback(int error, const char* description) {
+    SPDLOG_ERROR("GLFW errorno({:d}): {}", error, description);
+}
+
+int main(int args, char** argv) {
+    init_logger();
+
+    // Initializing GLFW
+    if (!glfwInit()) {
+        SPDLOG_CRITICAL("GLFW init failed");
+        return -1;
+    }
+
+    // Setting an error callback
+    glfwSetErrorCallback(glfw_error_callback);
+
+    // Creating a window and context
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef MacOS
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif // MacOS
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL-HelloTriangle", NULL, NULL);
     if (window == NULL) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
+        SPDLOG_CRITICAL("Failed to create GLFW window");
         glfwTerminate();
         return -2;
     }
 
-    // viewport
-    // glViewport(0, 0, 800, 600);
+    // Making the OpenGL context current
+    glfwMakeContextCurrent(window);
 
+    // Extension loader library initializing
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_CRITICAL("Failed to initialize GLAD");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -3;
+    }
+
+    // Checking the window close flag
     while (!glfwWindowShouldClose(window)) {
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -38,6 +73,8 @@ int main() {
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
+    // Terminating GLFW
     glfwTerminate();
 
     return 0;
