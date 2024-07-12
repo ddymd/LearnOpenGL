ddd62db5732c8233303fe6183c34c058ae628a90
junjun.dong <dongjunjun1013@126.com>
2024-07-12 17:34:33 +0800

    triangle

--- HelloTriangle.cpp
+++ HelloTriangle.cpp
@@ -8,6 +8,20 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"    FragColor = vec4(1.f, 0.5f, 0.2f, 1.f);\n"
+"}\0";
+
 void test() {
     SPDLOG_ERROR("error");
     SPDLOG_CRITICAL("critical");
@@ -66,6 +80,79 @@ void process_input(GLFWwindow* window) {
     }
 }
 
+bool init_triangle(unsigned int* VAO, unsigned int* shaderProgram) {
+    glGenVertexArrays(1, VAO);
+
+    // --- vertex input ---
+    float vertices[] = {
+        -0.5f, -0.5f, 0.f,
+        0.5f, -0.5f, 0.f,
+        0.f, 0.5f, 0.f
+    };
+    // vertex buffer objects
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // 1. bind Vertex Array Object
+    glBindVertexArray(*VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 3. then set our vertex attributes poniters
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // --- vertex shader ---
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // error checking
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        SPDLOG_ERROR("ERROR::SHADER::VERTEX::COMPILATION_FAILED {}", infoLog);
+        glDeleteShader(vertexShader);
+        return false;
+    }
+
+    // --- fragment shader ---
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // error checking
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        SPDLOG_ERROR("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED {}", infoLog);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        return false;
+    }
+
+    // --- shader program ---
+    *shaderProgram = glCreateProgram();
+    glAttachShader(*shaderProgram, vertexShader);
+    glAttachShader(*shaderProgram, fragmentShader);
+    glLinkProgram(*shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // error checking
+    glGetShaderiv(*shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(*shaderProgram, 512, NULL, infoLog);
+        SPDLOG_ERROR("ERROR::SHADER::PROGRAM::LINK_FAILED {}", infoLog);
+        return false;
+    }
+    return true;
+}
+
 int main(int args, char** argv) {
     init_logger();
 
@@ -108,13 +195,25 @@ int main(int args, char** argv) {
 
     // set resize callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glClearColor(0.2f, 0.3f, 0.3f, 0.02f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    // glClearColor(0.2f, 0.3f, 0.3f, 0.02f);
+    // glClear(GL_COLOR_BUFFER_BIT);
+    unsigned int VAO, shaderProgram;
+    init_triangle(&VAO, &shaderProgram);
+
     // Checking the window close flag
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
-        // glClearColor(0.2f, 0.3f, 0.3f, 0.02f);
+        glClearColor(0.2f, 0.3f, 0.3f, 0.02f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+        // glDrawArrays(GL_LINES, 0, 3);
+        // glDrawArrays(GL_LINE_LOOP, 0, 3);
+        // glDrawArrays(GL_LINE_STRIP, 0, 3);
+        // glDrawArrays(GL_POINTS, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
