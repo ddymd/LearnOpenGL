46f34e7a418b7207a8176a884fad20831351d070
junjun.dong <dongjunjun1013@126.com>
2024-07-26 07:19:00 +0800

    update camera exercises

--- Camera/Exercises/1.cpp
+++ Camera/Exercises/1.cpp
@@ -1,4 +1,6 @@
-
+// See if you can transform the camera class in such a way that it becomes a true fps camera
+// where you cannot fly; you can only look around while staying on the xz plane. Solution:
+// /src/1.getting_started/7.5.camera_exercise1/.
 int main(int argc, char** argv) {
     return 0;
 }
---
+++ Camera/Exercises/2.cpp
@@ -0,0 +1,7 @@
+// Try to create your own LookAt function where you manually create a view matrix as discussed
+// at the start of this chapter. Replace glm’s LookAt function with your own implementation
+// and see if it still acts the same. Solution: /src/1.getting_started/7.6.camera_
+// exercise2/.
+int main(int argc, char** argv) {
+    return 0;
+}
