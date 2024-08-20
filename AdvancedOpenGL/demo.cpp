e923c657797b59420d4fe9be5c064fc2ccce2e64
junjun.dong <dongjunjun1013@126.com>
2024-08-20 22:10:17 +0800

    update advanced opengl

---
+++ AdvancedOpenGL/demo.cpp
@@ -0,0 +1,86 @@
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "camera.hpp"
+#include "shader.hpp"
+#include "common.hpp"
+#include "advconf.hpp"
+
+#define SRC_VSHADER ADVOGL_SRC_DIR"shader.vs"
+#define SRC_FSHADER ADVOGL_SRC_DIR"shader.fs"
+
+Camera mcam(glm::vec3(0.f, 0.f, 1.f));
+
+int main(int argc, char* argv[]) {
+    if (!glfwInit()) {
+        SPDLOG_CRITICAL("GLFW Init Failed");
+        return -1;
+    }
+    SetCamera(&mcam);
+    glfwSetErrorCallback(GLFWErrorCB);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);    // for apple
+#endif // __APPLE__
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Advanced OpenGL", NULL, NULL);
+    if (!window) {
+        SPDLOG_CRITICAL("GLFW Create Window Failed");
+        return -2;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_CRITICAL("GLAD Load Proc Failed");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -3;
+    }
+
+    glfwSetFramebufferSizeCallback(window, GLFWFrameResizeCB);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, GLFWCursorPosCB);
+    glfwSetScrollCallback(window, GLFWScrollCB);
+    glEnable(GL_DEPTH_TEST);
+
+    unsigned int VAO[2], VBO[2];
+    glGenVertexArrays(2, VAO);
+    glBindVertexArray(VAO[0]);
+    glGenBuffers(2, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(VAO[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    Shader sp(SRC_VSHADER, SRC_FSHADER);
+    sp.use();
+
+    float lframe = 0.f;
+    while (!glfwWindowShouldClose(window)) {
+        float cframe = glfwGetTime();
+        ProcessInputs(window, cframe - lframe);
+        lframe = cframe;
+        glClearColor(0.1f, 0.1f, 0.1f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
