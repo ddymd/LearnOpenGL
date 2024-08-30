0380c002f7a9e6c9cb55d2828e81bc058f0024c0
junjun.dong <dongjunjun1013@126.com>
2024-08-30 17:31:02 +0800

    +cmake presets, updaste framebuffer

--- AdvancedOpenGL/Framebuffers/demo.cpp
+++ AdvancedOpenGL/Framebuffers/demo.cpp
@@ -4,20 +4,140 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "camera.hpp"
+#include "glm/ext/matrix_float4x4.hpp"
 #include "shader.hpp"
 #include "common.hpp"
 #include "advconf.hpp"
 
+#define SRC_VSHADER ADVOGL_SRC_DIR"shader.vs"
+#define SRC_FSHADER ADVOGL_SRC_DIR"shader.fs"
+
+float quadVertices[] = {
+ // positions // texCoords
+ -1.0f, 1.0f, 0.0f, 1.0f,
+ -1.0f,-1.0f, 0.0f, 0.0f,
+ 1.0f,-1.0f, 1.0f, 0.0f,
+ -1.0f, 1.0f, 0.0f, 1.0f,
+ 1.0f,-1.0f, 1.0f, 0.0f,
+ 1.0f, 1.0f, 1.0f, 1.0f
+};
+
 Camera mcam(glm::vec3(0.f, 0.f, 3.f));
-
 int main(int argc, char* argv[]) {
+    glfwSetErrorCallback(GLFWErrorCB);
+    if (!glfwInit()) {
+        SPDLOG_CRITICAL("GLFW Init Failed");
+        return -1;
+    }
+    SetCamera(&mcam);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);    // for apple
+#endif // __APPLE__
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Framebuffers", NULL, NULL);
+    if (!window) {
+        SPDLOG_CRITICAL("GLFW Create Window Failed");
+        return -2;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_CRITICAL("GLAD Load Proc Failed");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -3;
+    }
+    glfwSetFramebufferSizeCallback(window, GLFWFrameResizeCB);
+    glfwSetCursorPosCallback(window, GLFWCursorPosCB);
+    glfwSetScrollCallback(window, GLFWScrollCB);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    unsigned int tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
+    // create an framebuffer object and bind in
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    // create a texture image that we attach as a color attachment to the framebuffer
+    unsigned int texColorBuffer;
+    glGenTextures(1, &texColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+    // create renderbuffer object
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+    // check if framebuffer complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        SPDLOG_ERROR("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+    // load texture
+    unsigned int texcontainer = LoadTexture(TEXTURE_CONTAINER);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texcontainer);
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // program shader
+    Shader sp(SRC_VSHADER, SRC_FSHADER);
+    sp.use();
+    sp.setFloat("texture0", 0);
+    sp.setMat4("model", glm::mat4(1.f));
+
+    // glEnable(GL_DEPTH_TEST | GL_STENCIL_TEST);
+
+    float lframe = 0.f;
+    while (!glfwWindowShouldClose(window)) {
+        float cframe = glfwGetTime();
+        ProcessInputs(window, cframe - lframe);
+        lframe = cframe;
+
+        glm::mat4 view = mcam.GetViewMatrix();
+        glm::mat4 proj = glm::perspective(glm::radians(mcam.Zoom), 4.f / 3.f, .1f, 100.f);
+
+        // first pass
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        // drawscene();
+
+        // second pass
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.f, 1.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        sp.use();
+        sp.setMat4("view", view);
+        sp.setMat4("proj", proj);
+
+        glBindVertexArray(VAO);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
