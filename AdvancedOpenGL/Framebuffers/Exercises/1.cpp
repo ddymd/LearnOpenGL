3d5421e135b8b8e00af82c49a9210e4315210068
junjun.dong <dongjunjun1013@126.com>
2024-09-05 17:24:39 +0800

    +framebuffers exercises

---
+++ AdvancedOpenGL/Framebuffers/Exercises/1.cpp
@@ -0,0 +1,8 @@
+//  Canyouuseframebufferstocreatearear-viewmirror?Forthisyou’llhavetodrawyour
+//  scenetwice: onewiththecamerarotated180degreesandtheotherasnormal. Tryto
+//  createasmallquadat the topofyour screentoapplythemirror textureon. Solution:
+//  /src/4.advanced_opengl/5.2.framebuffers_exercise1/.
+
+int main(int argc, char** argv) {
+    return 0;
+}
