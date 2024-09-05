65d8c050ba484bd2df7e557d5896d107293f5fc1
junjun.dong <dongjunjun1013@126.com>
2024-09-05 16:42:48 +0800

    update framebuffers

--- AdvancedOpenGL/Framebuffers/screenshader.fs
+++ AdvancedOpenGL/Framebuffers/screenshader.fs
@@ -5,5 +5,12 @@ in vec2 fTexCoord;
 uniform sampler2D uTexture;
 
 void main() {
+    // FragColor = texture(uTexture, fTexCoord);
+    // inverse
+    // FragColor = vec4(vec3(1.0-texture(uTexture, fTexCoord)), 1.0);
+
+    // GrayScale
     FragColor = texture(uTexture, fTexCoord);
+    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    FragColor = vec4(average, average, average, 1.0);
 }
